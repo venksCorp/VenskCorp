79257755df6931a9503b57403af7dd1fbf6e628b
venksCorp <148593107+venksCorp@users.noreply.github.com>
2023-10-20 19:24:53 -0300

    commit inicial
    Primeiro site

---
+++ script.js
@@ -0,0 +1,252 @@
+var buttonP = document.getElementById("b-ativ")
+var buttonS =document.getElementById("b-ativs")
+var buttonT =document.getElementById("b-ativt")
+var buttonQ =document.getElementById("b-ativq")
+var tela =document.getElementById("tela")
+var tela2 =document.getElementById("telaS")
+var tela3 =document.getElementById("telaT")
+var tela4 =document.getElementById("telaQ")
+var menu =document.getElementById("menuu")
+var card = document.getElementById("cardp")
+var card2 = document.getElementById("cards")
+var card3 = document.getElementById("cardt")
+var card4 = document.getElementById("cardq")
+
+
+
+
+// JavaScript
+
+
+
+
+{
+buttonP.addEventListener("click",function(){
+
+if(tela.style.display === "block"){
+    tela.style.display ="none"
+
+}else{
+
+    tela.style.display ="block"
+
+
+
+}
+
+
+})
+
+buttonS.addEventListener("click",function(){
+
+    if(tela2.style.display === "block"){
+        tela2.style.display ="none"
+    
+    }else{
+    
+        tela2.style.display ="block"
+    
+    
+    
+    }
+    
+    
+    })
+
+    buttonT.addEventListener("click",function(){
+
+        if(tela3.style.display === "block"){
+            tela3.style.display ="none"
+        
+        }else{
+        
+            tela3.style.display ="block"
+        
+        
+        
+        }
+        
+        
+        })
+    
+    }
+
+    buttonQ.addEventListener("click",function(){
+
+        if(tela4.style.display === "block"){
+            tela4.style.display ="none"
+        
+        }else{
+        
+            tela4.style.display ="block"
+        
+        
+        
+        }
+        
+        
+        })
+
+
+
+{
+
+buttonP.addEventListener("click",function(){
+
+if(tela.style.display === "block"){
+
+ card.style.display ="none"
+ card2.style.display ="none"
+ card3.style.display ="none"
+ card4.style.display ="none"
+
+}else{
+
+    card.style.display ="block"
+    card2.style.display ="block"
+    card3.style.display ="block"
+    card4.style.display ="block"
+
+}
+
+
+
+})
+
+buttonS.addEventListener("click",function(){
+
+    if(tela2.style.display === "block"){
+    
+    card.style.display ="none"
+    card2.style.display ="none"
+    card3.style.display ="none"
+    card4.style.display ="none"
+    
+    }else{
+    
+        card.style.display ="block"
+        card2.style.display ="block"
+        card3.style.display ="block"
+        card4.style.display ="block"
+    
+    }
+    
+    
+    
+    })
+
+
+
+buttonT.addEventListener("click",function(){
+
+    if(tela3.style.display === "block"){
+    
+    card.style.display ="none"
+    card2.style.display ="none"
+    card3.style.display ="none"
+    card4.style.display ="none"
+    
+    }else{
+    
+        card.style.display ="block"
+        card2.style.display ="block"
+        card3.style.display ="block"
+        card4.style.display ="block"
+    
+    }
+    
+    
+    
+    })
+
+    buttonQ.addEventListener("click",function(){
+
+        if(tela4.style.display === "block"){
+        
+        card.style.display ="none"
+        card2.style.display ="none"
+        card3.style.display ="none"
+        card4.style.display ="none"
+        
+        }else{
+        
+            card.style.display ="block"
+            card2.style.display ="block"
+            card3.style.display ="block"
+            card4.style.display ="block"
+        
+        }
+        
+        
+        
+        })
+
+
+}
+
+{
+
+var fecha=document.getElementById("closeu")
+var fecha2=document.getElementById("closee")
+var fecha3=document.getElementById("closeee")
+var fecha4=document.getElementById("closs")
+
+closeee
+
+fecha.addEventListener("click", function(){
+
+    if(tela.style.display === "block"){
+
+        tela.style.display = "none"
+        card.style.display ="block"
+        card2.style.display ="block"
+        card3.style.display ="block"
+        card4.style.display ="block"
+
+    }
+
+})
+fecha2.addEventListener("click", function(){
+
+    if(tela2.style.display === "block"){
+
+        tela2.style.display = "none"
+        card.style.display ="block"
+        card2.style.display ="block"
+        card3.style.display ="block"
+        card4.style.display ="block"
+
+    }
+
+})
+
+fecha3.addEventListener("click", function(){
+
+    if(tela3.style.display === "block"){
+
+        tela3.style.display = "none"
+        card.style.display ="block"
+        card2.style.display ="block"
+        card3.style.display ="block"
+        card4.style.display ="block"
+
+    }
+
+})
+fecha4.addEventListener("click", function(){
+
+    if(tela4.style.display === "block"){
+
+        tela4.style.display = "none"
+        card.style.display ="block"
+        card2.style.display ="block"
+        card3.style.display ="block"
+        card4.style.display ="block"
+
+    }
+
+})
+
+}
+
+
